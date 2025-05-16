a8365d94a661f31d1591766964f77bf718f054ba
santogarrido <santocildesgarridoraul@gmail.com>
2025-05-16 12:19:57 +0200

    Por si merge

--- src/view/EditVehiculoView.java
+++ src/view/EditVehiculoView.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
@@ -28,7 +29,6 @@ public class EditVehiculoView extends JFrame {
 	private Usuario usuarioActivo;
 	private final VehiculoController controller = new VehiculoController();
 
-
 	public EditVehiculoView(Vehiculo v, Usuario u) {
 		super("Editar vehículo");
 		this.vehiculoSeleccionado = v;
@@ -38,7 +38,6 @@ public class EditVehiculoView extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		
 		lblMatricula = new JLabel("Matrícula");
 		lblMatricula.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		lblMatricula.setBounds(10, 10, 70, 22);
@@ -58,13 +57,13 @@ public class EditVehiculoView extends JFrame {
 		txtMarca = new JTextField();
 		txtMarca.setText(vehiculoSeleccionado.getMarca());
 		txtMarca.setBounds(110, 60, 62, 20);
-		
+
 		guardar = new JButton("Guardar");
 		guardar.setBounds(10, 164, 101, 23);
 		volver = new JButton("Volver");
 		volver.setBounds(121, 164, 78, 23);
 		contentPane.setLayout(null);
-		
+
 		contentPane.add(lblMatricula);
 		contentPane.add(lblMarca);
 		contentPane.add(lblModelo);
@@ -73,47 +72,56 @@ public class EditVehiculoView extends JFrame {
 		contentPane.add(txtModelo);
 		contentPane.add(guardar);
 		contentPane.add(volver);
-		
+
 		ManejadorEventos m = new ManejadorEventos();
 		guardar.addActionListener(m);
 		volver.addActionListener(m);
-		
+
 		setContentPane(contentPane);
 		setVisible(true);
 		setLocationRelativeTo(null);
 	}
 
-	private class ManejadorEventos implements ActionListener{
+	private class ManejadorEventos implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
+
 			Object o = e.getSource();
-			
-			if(o == guardar) {
-				vehiculoSeleccionado.setMatricula(txtMatricula.getText());
-				vehiculoSeleccionado.setMarca(txtMarca.getText());
-				vehiculoSeleccionado.setModelo(txtModelo.getText());
-				
-				try {
-					controller.save(Conexion.obtener(), vehiculoSeleccionado);
-					dispose();
-					new GestionarVehiculosView(usuarioActivo);
-				} catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+
+			if (o == guardar) {
+				if (!txtMatricula.getText().isEmpty() && !txtModelo.getText().isEmpty()
+						&& !txtMarca.getText().isEmpty()) {
+
+					if (txtMatricula.getText().matches("[0-9]{4}[-][^aeiouAEIOUa-z]{3}")) {
+						Vehiculo v = new Vehiculo(usuarioActivo.getId(), txtMatricula.getText(), txtModelo.getText(),
+								txtMarca.getText(), 100, false);
+
+						try {
+							controller.save(Conexion.obtener(), v);
+						} catch (ClassNotFoundException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						dispose();
+						new GestionarVehiculosView(usuarioActivo);
+						JOptionPane.showMessageDialog(null, "Vehículo registrado con éxito", "Vehículo registrado",
+								JOptionPane.INFORMATION_MESSAGE);
+					} else {
+						JOptionPane.showMessageDialog(null, "Formato de la matricula incorrecto 1111-BBB", "Error",
+								JOptionPane.ERROR_MESSAGE);
+					}
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Hay campos que están vacios", "Error",
+							JOptionPane.ERROR_MESSAGE);
 				}
-				
-			}else if(o == volver) {
-				dispose();
-				new GestionarVehiculosView(usuarioActivo);
+
 			}
-			
+
 		}
-		
 	}
-	
 }
